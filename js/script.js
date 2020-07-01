61a7f68355abf7ddd930e02043a7b444f04a6651
vkudok <vladkutepov1@gmail.com>
2020-07-02 01:12:52 +0300

    bad полностью готов

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ var rellax = new Rellax('.rellax', {
       console.log(position);
     }
   });
+
+  //scrollTo
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    var plus = $(window).innerHeight() / 100 * 25;
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - (plus/2)
+        }, 1000);
+    }
+    // var widthWindow = $(window).width();
+    // if (widthWindow <= 1024){
+    //   $('header').fadeToggle(400);
+    // } 
+});
